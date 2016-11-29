303f21c5986cfd050e9674642e45b70446b5a7c3
georges <gderemusat@gmail.com>
2016-11-29 21:06:04 +0100

    .

--- index.php
+++ index.php
@@ -11,31 +11,32 @@
 	<div id="app">
 		<div class="main">
 			<div class="preview">	
-				<div class="prev1-sidebar"></div>
-				<img src="img/default-user-avatar.png" class="prev-photo"/>
-				<div class="prev1-header">
+				<div class="prev{{themeNumber}}-sidebar"></div>
+				<img src="img/default-user-avatar.png" class="prev{{themeNumber}}-photo"/>
+				<div class="prev{{themeNumber}}-img"></div>
+				<div class="prev{{themeNumber}}-header">
 					<p>Nom: {{name}}</p>
 					<p>Prénom: {{first_name}}</p>	
 					<p>Mail: {{email}}</p>	
 					<p>Tel: {{tel}}</p>	
 					<p>Date de naissance: {{date_naissance}}</p>	
 				</div>
-				<div class="prev1-formations">
-					<div class="prev1-section-name">Formations</div>
+				<div class="prev{{themeNumber}}-formations">
+					<div class="prev{{themeNumber}}-section-name">Formations</div>
 					<div v-for="formation in formations">
 						<p class="date">{{formation.debut}}-{{formation.fin}}</p>
 						<p>{{formation.desc}}</p>
 					</div>
 				</div>
-				<div class="prev1-exp">	
-					<div class="prev1-section-name">Expériences professionnelles</div>
+				<div class="prev{{themeNumber}}-exp">	
+					<div class="prev{{themeNumber}}-section-name">Expériences professionnelles</div>
 					<div v-for="exp_pro in exps_pro">
 						<p class="date">{{exp_pro.debut}}-{{exp_pro.fin}}</p>
 						<p>{{exp_pro.desc}}</p>
 					</div>
 				</div>
-				<div class="prev1-comp">	
-					<div class="prev1-section-name">Langues et compétences</div>
+				<div class="prev{{themeNumber}}-comp">	
+					<div class="prev{{themeNumber}}-section-name">Langues et compétences</div>
 					<div v-for="competence in competences">
 						<p>{{competence.desc}}</p>
 					</div>
@@ -43,7 +44,15 @@
 			</div>
 		</div>
 		<div class="sidebar">
-			<form action="generator.php" method="post" enctype="multipart/form-data">	
+			<form action="generator.php" method="post" enctype="multipart/form-data">
+
+				<h2>Thèmes</h2>
+
+				<div class="form-group">
+					<label for="theme1" class="radio-inline"><input type="radio" name="theme" id="theme1" value="1" @click="changeTheme(1)"></label>
+					<label for="theme2" class="radio-inline"><input type="radio" name="theme" id="theme2" value="2" @click="changeTheme(2)"></label>
+					<label for="theme3" class="radio-inline"><input type="radio" name="theme" id="theme3" value="3" @click="changeTheme(3)"></label>
+				</div>
 
 				<h2>Infos personnelles</h2>	
 
@@ -129,9 +138,10 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.16/vue.js"></script>
 	<script>
 		
-		new Vue({
+		app = new Vue({
 			el: "#app",
 			data:{
+				themeNumber: 2,
 				size: 0,
 				formations:[{
 					debut : '',
@@ -174,6 +184,9 @@
    				},
    				maxSize(){
    					alert('Vous avez atteint le nombre maximum de champs!');
+   				},
+   				changeTheme: function (themeNumber) {
+   					this.themeNumber = themeNumber;
    				}
 			}
 		});
